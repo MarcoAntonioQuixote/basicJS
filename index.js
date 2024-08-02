15a66d6eb2a5760828daf85046334a6b6bf2335d
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-08-01 21:59:37 -0600

    Mark Anthony rocks

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-
-
-
-let shirts = 100;
-let pants = 200;
-let shoes = 50;
-
-let discount = prompt('What discount would you like?')
-
-
-let subtotal = shirts + shoes + pants;
-
-let total = subtotal * discount;
-
+console.log('Mark Anthony 🪨🪨')
